4c797ab0cbbea04e0de70836f828f86950078779
vikasomu <mishra.vikasomu@gmail.com>
2023-12-22 01:37:15 +0530

    create a tst api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import userRouter from "./routes/user.route.js"
 dotenv.config()
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("connection establish to MONGO DB")
@@ -13,3 +14,5 @@ const app=express()
 app.listen(3000,()=>{
     console.log("serving is running on port 3000")
 })
+
+app.use("/api/user",userRouter)

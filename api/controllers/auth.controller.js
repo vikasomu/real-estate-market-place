e3fbde10c9d0651b4cfd7ad3315c3e3e95da6f76
vikasomu <mishra.vikasomu@gmail.com>
2023-12-22 23:45:36 +0530

    create a middlewear to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import { error } from "console";
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs"
-export const Signup=async (req,res)=>{
+export const Signup=async (req,res,next)=>{
    const { username, email, password }=req.body
    const hashedPassword=bcryptjs.hashSync(password,10)
    const newUser = new User({ username, email, password:hashedPassword });
@@ -10,7 +10,8 @@ export const Signup=async (req,res)=>{
        res.status(201).json("user created successfully.")
    }
    catch(error){
-    console.log(error.message,"msg")
-    res.status(500).json(error.message)
+    next(error)
+    // console.log(error.message,"msg")
+    // res.status(500).json(error.message)
    }
 }
